034f1b110ca0ddf945c884e56267c4b762885f30
Joel Scarfone <joel.scarfone@carleton.ca>
2016-12-05 02:23:18 -0500

    Removed unnessesary lines

--- lib/elgamal.rb
+++ lib/elgamal.rb
@@ -1,26 +1,3 @@
-# require 'bigdecimal' unless defined?(BigDecimal)
-# require 'stringio'   unless defined?(StringIO)
-
-# if RUBY_VERSION < '1.9.1'
-#   # @see http://rubygems.org/gems/backports
-#   begin
-#     require 'backports/1.9.1'
-#   rescue LoadError
-#     abort "RSA.rb requires Ruby 1.9.1 or the Backports gem (hint: `gem install backports')."
-#   end
-# end
-
-# module ElGamal
-#   # autoload :Math,    'rsa/math'
-#   # autoload :PKCS1,   'rsa/pkcs1'
-#   autoload :PrivateKey,     'elgamal/private_key'
-#   autoload :PublicKey,     'elgamal/public_key'
-#   autoload :KeyPair, 'elgamal/key_pair'
-#   # autoload :OpenSSL, 'rsa/openssl'
-#   # autoload :VERSION, 'rsa/version'
-#   # autoload :KeyPair, 'rsa/version'
-# end
-
 require 'openssl'
 require_relative 'elgamal/key_pair'
 require_relative 'elgamal/private_key'
